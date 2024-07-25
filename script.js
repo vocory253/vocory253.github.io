35f676a067c24fb2c933d605ef393ac84fb04753
Cory Vo <cvo253@uw.edu>
2024-07-25 02:07:48 -0700

    first-big

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-// JavaScript for typewriter effect
-const text = document.getElementById('intro_message').textContent.trim();
-const speed = 50; // Adjust typing speed in milliseconds
 
-document.getElementById('intro_message').textContent = '';
+document.addEventListener("DOMContentLoaded", function() {
+  // Code for adding 'active' class to current page link
+  var currentUrl = window.location.href;
+  var links = document.querySelectorAll('.navbar');
+  
+  links.forEach((link) => {
+      if (link.href === currentUrl) {
+          link.classList.add('active');
+      }
+  });
 
-let i = 0;
-function typeWriter() {
-  if (i < text.length) {
-    document.getElementById('intro_message').textContent += text.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+  // Check if we're on the projects page
+  if (window.location.pathname.includes('project.html')) {
+    // Typewriter effect code
+    const introMessage = document.getElementById('intro_message');
+    if (introMessage) {
+      const text = introMessage.textContent.trim();
+      const speed = 50; // Adjust typing speed in milliseconds
+
+      introMessage.textContent = '';
+
+      let i = 0;
+      function typeWriter() {
+        if (i < text.length) {
+          introMessage.textContent += text.charAt(i);
+          i++;
+          setTimeout(typeWriter, speed);
+        }
+      }
+
+      // Start typing animation
+      typeWriter();
+    }
   }
-}
-
-// Start typing animation
-typeWriter();
+});
